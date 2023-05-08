4d9972015d196086c8329ff0d1958a9ee0466a09
Morris Chiu <xerviam@icloud.com>
2023-05-08 15:04:46 +0800

    init project

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { inject } from 'vue';
+import axios from '@/utils/axios';
+inject('axios', axios)
+axios.get('/')
+    .then(res => {
+        alert('api ok')
+    })
+    .catch(err => {
+        alert('api error')
+    })
+</script>
+<template>
+    <h1>testing api</h1>
+</template>
