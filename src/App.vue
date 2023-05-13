a5cca70845e07f0186384e3d44ed6948c5f9ac82
Morris Chiu <xerviam@icloud.com>
2023-05-13 19:43:23 +0800

    feat: landing page

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <script setup>
+import { computed } from 'vue'
 import { RouterView } from 'vue-router';
+import { useRoute } from 'vue-router';
+const route = useRoute()
+const extendsLayout = computed(() => route.meta.extendsLayout)
 </script>
 
 <template>
-  <RouterView />
+  <template v-if="extendsLayout">
+    <component :is="extendsLayout">
+      <RouterView />
+    </component>
+  </template>
+  <template v-else>
+    <RouterView />
+  </template>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import BaseLayout from '@/layouts/Base.vue'
 
 const routes = [
     {
         path: '/',
         name: 'Home',
-        component: () => import('@/views/Home.vue')
-    }
+        component: () => import('@/views/Home.vue'),
+        meta: {
+            requiresAuth: false,
+            extendsLayout: BaseLayout,
+
+        }
+    },
 ]
 
 const router = createRouter({
