57eda48225ab3b42de77ce29be8e8d0c5f41b38f
Morris Chiu <xerviam@icloud.com>
2023-05-18 20:31:29 +0800

    feat: user login page

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,64 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import BaseLayout from '@/layouts/Base.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import BaseLayout from "@/layouts/Base.vue";
+import Cookies from "js-cookie";
+import { useUserStore } from "../stores/user";
 
 const routes = [
-    {
-        path: '/',
-        name: 'Home',
-        component: () => import('@/views/Home.vue'),
-        meta: {
-            requiresAuth: false,
-            extendsLayout: BaseLayout,
-
-        }
+  {
+    path: "/",
+    name: "Home",
+    component: () => import("@/views/Home.vue"),
+    meta: {
+      requiresAuth: false,
+      extendsLayout: BaseLayout,
     },
-]
+  },
+  {
+    path: "/sign-in",
+    name: "SignIn",
+    component: () => import("@/views/SignIn.vue"),
+    meta: {
+      requiresAuth: false,
+    },
+  },
+  {
+    path: "/sign-up",
+    name: "SignUp",
+    component: () => import("@/views/SignUp.vue"),
+    meta: {
+      requiresAuth: false,
+    },
+  },
+  {
+    path: "/profile",
+    name: "Profile",
+    component: () => import("@/views/Profile.vue"),
+    meta: {
+      requiresAuth: true,
+      extendsLayout: BaseLayout,
+    },
+  },
+];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes,
-})
+  history: createWebHistory(),
+  routes,
+});
 
-export default router
+router.beforeEach(async (to, from, next) => {
+  const token = Cookies.get("access_token");
+  if (token && !useUserStore().isAuthenticated) {
+    await useUserStore().authUser();
+  }
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    if (!token) {
+      next({ name: "SignIn" });
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
+});
+
+export default router;
--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,13 +1,46 @@
-import axios from 'axios'
+import axios from "axios";
+import Cookies from "js-cookie";
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
-    timeout: import.meta.env.NODE_ENV === 'development' 
-    ? 0 : 30000,
-    headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-    }
-})
+  baseURL: import.meta.env.VITE_API_URL,
+  timeout: import.meta.env.NODE_ENV === "development" ? 0 : 30000,
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+});
 
-export default axiosInstance
+axiosInstance.interceptors.request.use(
+  (config) => {
+    let token = Cookies.get("access_token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    console.groupCollapsed(
+      `%c${response.config.method.toUpperCase()} - ${response.config.url}`,
+      "color: #00A7F7; font-weight: bold;"
+    );
+    console.log(response);
+    console.groupEnd();
+    let result = response.data;
+    if (result.success === true) {
+      return result.data;
+    } else {
+      throw new Error(result.message);
+    }
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
